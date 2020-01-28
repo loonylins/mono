e164f4aec06290fa12e82816c6641a09ee66253b
aspiering <aspiering@gmail.com>
2020-01-28 14:56:17 -0800

    trying  64bit only here, since right now you can't even build 2017.4 on 10.15

--- external/buildscripts/build_classlibs_osx.pl
+++ external/buildscripts/build_classlibs_osx.pl
@@ -74,9 +74,9 @@ if (not $skipbuild)
 
 	$ENV{'CC'} = "$sdkPath/../usr/bin/clang";
 	$ENV{'CXX'} = "$sdkPath/../usr/bin/clang++";
-	$ENV{CFLAGS}  = "$ENV{CFLAGS} -arch i386 -D_XOPEN_SOURCE";
+	$ENV{CFLAGS}  = "$ENV{CFLAGS} -arch x86_64 -D_XOPEN_SOURCE";
 	$ENV{CXXFLAGS}  = "$ENV{CXXFLAGS} $ENV{CFLAGS}";
-	$ENV{LDFLAGS}  = "$ENV{LDFLAGS} -arch i386";
+	$ENV{LDFLAGS}  = "$ENV{LDFLAGS} -arch x86_64";
 	if ($^O eq 'darwin')
 	{
 		$ENV{'MACSDKOPTIONS'} = "$ENV{CFLAGS} -mmacosx-version-min=$macversion -isysroot $sdkPath";
